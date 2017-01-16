ddc2777fce6852f25577cc94e620549dc2206e27
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-01-16 13:01:01 +0100

    Legger til dokumentasjon for oppretting av trust store

---
+++ sdp-klient-v5/src/main/java/no/difi/sdp/Dokumentasjon.java
@@ -0,0 +1,228 @@
+package no.difi.sdp;
+
+import no.difi.sdp.client2.KlientKonfigurasjon;
+import no.difi.sdp.client2.SendResultat;
+import no.difi.sdp.client2.SikkerDigitalPostKlient;
+import no.difi.sdp.client2.domain.*;
+import no.difi.sdp.client2.domain.digital_post.DigitalPost;
+import no.difi.sdp.client2.domain.digital_post.EpostVarsel;
+import no.difi.sdp.client2.domain.digital_post.Sikkerhetsnivaa;
+import no.difi.sdp.client2.domain.digital_post.SmsVarsel;
+import no.difi.sdp.client2.domain.exceptions.SendException;
+import no.difi.sdp.client2.domain.exceptions.SertifikatException;
+import no.difi.sdp.client2.domain.fysisk_post.*;
+import no.difi.sdp.client2.domain.kvittering.*;
+import no.digipost.api.representations.Organisasjonsnummer;
+import sun.plugin2.message.Message;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.X509Certificate;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Arrays.asList;
+
+@SuppressWarnings("Duplicates")
+public class Dokumentasjon {
+
+    public static void digitalPost() {
+        Sertifikat mottakerSertifikat = null;   //Fås fra Oppslagstjenesten
+        String orgnrPostkasse = null;           //Fås fra Oppslagstjenesten
+        String postkasseadresse = null;         //Fås fra Oppslagstjenesten
+
+        Mottaker mottaker = Mottaker
+                .builder(
+                        "99999999999",
+                        postkasseadresse,
+                        mottakerSertifikat,
+                        Organisasjonsnummer.of(orgnrPostkasse))
+                .build();
+
+
+        SmsVarsel smsVarsel = SmsVarsel.builder("4799999999",
+                "Du har mottatt brev i din digitale postkasse")
+                .build();
+
+        EpostVarsel epostVarsel = EpostVarsel.builder("epost@example.com",
+                "Du har mottatt brev i din digitale postkasse")
+                .varselEtterDager(asList(1, 4, 10))
+                .build();
+
+        DigitalPost digitalPost = DigitalPost
+                .builder(mottaker, "Ikke-sensitiv tittel")
+                .virkningsdato(new Date())
+                .aapningskvittering(false)
+                .sikkerhetsnivaa(Sikkerhetsnivaa.NIVAA_3)
+                .epostVarsel(epostVarsel)
+                .smsVarsel(smsVarsel)
+                .build();
+    }
+
+    public static void fysiskPost() {
+        Sertifikat utskriftsleverandørSertifikat = null;    //Printsertifikat fra Oppslagstjenesten
+        TekniskMottaker utskriftsleverandør =
+                new TekniskMottaker(Organisasjonsnummer.of("99999999"), utskriftsleverandørSertifikat);
+
+        FysiskPost fysiskPost = FysiskPost.builder()
+                .adresse(
+                        KonvoluttAdresse.build("Ola Nordmann")
+                                .iNorge("Fjellheimen 22", "", "", "0001", "Oslo")
+                                .build())
+                .retur(
+                        Returhaandtering.DIREKTE_RETUR.MAKULERING_MED_MELDING,
+                        KonvoluttAdresse.build("Returkongen")
+                                .iNorge("Returveien 3", "", "", "0002", "Oslo")
+                                .build())
+                .sendesMed(Posttype.A_PRIORITERT)
+                .utskrift(Utskriftsfarge.FARGE, utskriftsleverandør)
+                .build();
+    }
+
+    public static void opprettForsendelse() {
+        DigitalPost digitalPost = null; //Som initiert tidligere
+
+        Dokument hovedDokument = Dokument
+                .builder("Sensitiv brevtittel", new File("/sti/til/dokument"))
+                .mimeType("application/pdf")
+                .build();
+
+        Dokumentpakke dokumentpakke = Dokumentpakke.builder(hovedDokument)
+                .vedlegg(new ArrayList<Dokument>())
+                .build();
+
+        AvsenderOrganisasjonsnummer avsenderOrgnr =
+                AktoerOrganisasjonsnummer.of("999999999").forfremTilAvsender();
+
+        Avsender avsender = Avsender
+                .builder(avsenderOrgnr)
+                .build();
+
+        Forsendelse forsendelse = Forsendelse
+                .digital(avsender, digitalPost, dokumentpakke)
+                .konversasjonsId(UUID.randomUUID().toString())
+                .prioritet(Prioritet.NORMAL)
+                .mpcId("KøId")
+                .spraakkode("NO")
+                .build();
+    }
+
+    public static void OppretteKlientOgSendPost() {
+        Forsendelse forsendelse = null;         //Som initiert tidligere
+        KeyStore virksomhetssertifikat = null;  //Last inn sertifikat her.
+
+        KlientKonfigurasjon klientKonfigurasjon = KlientKonfigurasjon
+                .builder(Miljo.FUNKSJONELT_TESTMILJO)
+                .connectionTimeout(20, TimeUnit.SECONDS)
+                .build();
+
+        DatabehandlerOrganisasjonsnummer databehandlerOrgnr =
+                AktoerOrganisasjonsnummer.of("555555555").forfremTilDatabehandler();
+
+        Databehandler databehandler = Databehandler
+                .builder(
+                        databehandlerOrgnr,
+                        Noekkelpar.fraKeyStoreUtenTrustStore(
+                                virksomhetssertifikat,
+                                "sertifikatAlias",
+                                "sertifikatPassord"))
+                .build();
+
+        SikkerDigitalPostKlient sikkerDigitalPostKlient = new SikkerDigitalPostKlient(databehandler, klientKonfigurasjon);
+
+        try {
+            sikkerDigitalPostKlient.send(forsendelse);
+        } catch (SendException sendException) {
+            SendException.AntattSkyldig antattSkyldig = sendException.getAntattSkyldig();
+            String message = sendException.getMessage();
+        }
+    }
+
+    public static void HentKvitteringOgBekreft() {
+        SikkerDigitalPostKlient sikkerDigitalPostKlient = null;     //Som initiert tidligere
+
+        KvitteringForespoersel kvitteringForespoersel = KvitteringForespoersel.builder(Prioritet.NORMAL).mpcId("KøId").build();
+        ForretningsKvittering forretningsKvittering = sikkerDigitalPostKlient.hentKvittering(kvitteringForespoersel);
+
+        if (forretningsKvittering instanceof LeveringsKvittering) {
+            //Forsendelse er levert til digital postkasse
+        } else if (forretningsKvittering instanceof AapningsKvittering) {
+            //Forsendelse ble åpnet av mottaker
+        } else if (forretningsKvittering instanceof MottaksKvittering) {
+            //Kvittering på sending av fysisk post
+        } else if (forretningsKvittering instanceof ReturpostKvittering) {
+            //Forsendelse er blitt sendt i retur
+        } else if (forretningsKvittering instanceof Feil) {
+            //Feil skjedde under sending
+        }
+
+        sikkerDigitalPostKlient.bekreft(forretningsKvittering);
+    }
+
+    public static void HentAntallFakturerbareBytes(){
+        SikkerDigitalPostKlient sikkerDigitalPostKlient = null;     //Som initiert tidligere
+        Forsendelse forsendelse = null;                             //Som initiert tidligere
+
+        SendResultat sendResultat = sikkerDigitalPostKlient.send(forsendelse);
+        long antallFakturerbareBytes = sendResultat.getFakturerbareBytes();
+    }
+
+    public static void noekkelpar_fra_key_store_uten_trust_store(){
+        KeyStore keyStore;
+        String virksomhetssertifikatsti = "/sti/til/virksomhetssertifikat";
+        String virksomhetssertifikatpassord = "virksomhetssertifikat_passord";
+        try {
+            keyStore = KeyStore.getInstance("PKCS12");
+            keyStore.load(
+                    new FileInputStream(virksomhetssertifikatsti),
+                    virksomhetssertifikatpassord.toCharArray()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    MessageFormat.format(
+                            "Fant ikke virksomhetssertifikat på sti {0}.",
+                            virksomhetssertifikatsti), e
+            );
+        }
+    }
+
+    public static void noekkelpar_fra_key_store_og_trust_store(){
+        KeyStore trustStore;
+
+        try {
+            trustStore = KeyStore.getInstance("JCEKS");
+            trustStore.load(null, "".toCharArray());
+        } catch (Exception e) {
+            throw new SertifikatException(
+                    MessageFormat.format(
+                            "Oppretting av tom keystore feilet. Grunnen er {0}.",
+                            e.toString()
+                    )
+            );
+        }
+
+        List<X509Certificate> certificates = null; //Sertifikater lastet fra disk
+
+        for(X509Certificate cert : certificates){
+            String uniqueCertificateAlias = cert.getSerialNumber().toString() + Math.random();
+
+            try {
+                trustStore.setCertificateEntry(uniqueCertificateAlias, cert);
+            } catch (KeyStoreException e) {
+                throw new RuntimeException(
+                        MessageFormat.format(
+                                "Klarte ikke å legge til sertifikat til trust store. Grunnen er {0}",
+                                e.toString()
+                        )
+                );
+
+            }
+        }
+    }
+}
